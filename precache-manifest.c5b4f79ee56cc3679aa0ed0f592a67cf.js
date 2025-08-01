6415ce732a5152eeb0c73c1c11a5a9f17f4f08db
Melis34 <Melis34@users.noreply.github.com>
2025-08-01 11:54:14 +0000

    deploy: MelisITSolutions/ALL-Active-IT-WEB@edf5bd2aec6a59feafe15a578074e082f1a2001f

---
+++ precache-manifest.c5b4f79ee56cc3679aa0ed0f592a67cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c66892e821df1770013",
+    "url": "/static/css/main.e7f7d693.chunk.css"
+  },
+  {
+    "revision": "7c66892e821df1770013",
+    "url": "/static/js/main.2387db8d.chunk.js"
+  },
+  {
+    "revision": "87e7c156bd216034e521",
+    "url": "/static/js/runtime~main.727c92dd.js"
+  },
+  {
+    "revision": "e3c6fa22f1752de76b94",
+    "url": "/static/css/2.2c3fd4f0.chunk.css"
+  },
+  {
+    "revision": "e3c6fa22f1752de76b94",
+    "url": "/static/js/2.5eac7d25.chunk.js"
+  },
+  {
+    "revision": "50ee3c8b5b065e8c8cba",
+    "url": "/static/js/3.26d8ba2d.chunk.js"
+  },
+  {
+    "revision": "cabddb3abdc8fab0bf5e7c057e5f2d8c",
+    "url": "/static/media/Logo-MTB-Oosterhout_eind.cabddb3a.png"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/static/media/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "8a9fa02cc30ea189e6fdbf02835ec87b",
+    "url": "/static/media/MTBO.Banner.8a9fa02c.png"
+  },
+  {
+    "revision": "a4b0cd1c1fc490c7a01e5e90d806f628",
+    "url": "/static/media/Logo All Active IT met tekst wit.a4b0cd1c.png"
+  },
+  {
+    "revision": "420e6395cc62f17f225554b35989b98d",
+    "url": "/static/media/Logo All Active IT met tekst licht geel.420e6395.png"
+  },
+  {
+    "revision": "80c6af8f0d7df40b01933e1d829d0f06",
+    "url": "/index.html"
+  }
+];
