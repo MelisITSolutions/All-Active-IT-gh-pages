42ab61b167aa4f9b2ae535273ee2118c82c89a0d
Melis34 <Melis34@users.noreply.github.com>
2025-08-05 14:20:26 +0000

    deploy: MelisITSolutions/ALL-Active-IT-WEB@729013f743ce16fd6b98d4be0330d2c97eafa596

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ec783a1a33feb7f8de730f5bc6c2e37.js"
+  "/precache-manifest.7a6b678047389800a91b711140275726.js"
 );
 
 workbox.clientsClaim();
