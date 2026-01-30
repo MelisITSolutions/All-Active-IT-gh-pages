ad81123771f76f25494c4f2c9974243efa501029
Melis34 <Melis34@users.noreply.github.com>
2026-01-30 16:10:59 +0000

    deploy: MelisITSolutions/ALL-Active-IT-WEB@e28857abe269103f60b28791c1a462e93649052c

--- precache-manifest.e832d3f8a54517be3f15ba8dc790bab2.js
+++ precache-manifest.e832d3f8a54517be3f15ba8dc790bab2.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "1188ca8eee672103887a",
+    "revision": "9f147bc657c0a0c466b4",
     "url": "/static/css/main.e7f7d693.chunk.css"
   },
   {
-    "revision": "1188ca8eee672103887a",
-    "url": "/static/js/main.0fa6eb0d.chunk.js"
+    "revision": "9f147bc657c0a0c466b4",
+    "url": "/static/js/main.2a9bd464.chunk.js"
   },
   {
-    "revision": "65a46ad71e900a420cf9",
-    "url": "/static/js/runtime~main.67748284.js"
+    "revision": "c8fb4a733ef0f8d697ef",
+    "url": "/static/js/runtime~main.448995b9.js"
   },
   {
-    "revision": "57022e9f94e802a999af",
+    "revision": "983292bc99ffdc4465ff",
     "url": "/static/css/2.2c3fd4f0.chunk.css"
   },
   {
-    "revision": "57022e9f94e802a999af",
-    "url": "/static/js/2.16e52e9c.chunk.js"
+    "revision": "983292bc99ffdc4465ff",
+    "url": "/static/js/2.818882dc.chunk.js"
   },
   {
-    "revision": "da2ab402e4728d49388f",
-    "url": "/static/js/3.3bf79c99.chunk.js"
+    "revision": "dfef9796bf5bec4e5899",
+    "url": "/static/js/3.4efdd5b5.chunk.js"
+  },
+  {
+    "revision": "6822f7e5463b648bbdcc8138994e2187",
+    "url": "/static/media/Logo All Active IT met tekst_in lettercontouren.6822f7e5.png"
   },
   {
     "revision": "cabddb3abdc8fab0bf5e7c057e5f2d8c",
@@ -32,24 +36,24 @@ self.__precacheManifest = [
     "url": "/static/media/color.c7a33805.png"
   },
   {
-    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
-    "url": "/static/media/primeicons.c08d00ca.eot"
-  },
-  {
     "revision": "c0929a1e055f2ceb5c889501c24a24e1",
     "url": "/static/media/primeicons.c0929a1e.woff2"
-  },
-  {
-    "revision": "acb19296cb26b15dcf42e13c603217f2",
-    "url": "/static/media/primeicons.acb19296.ttf"
   },
   {
     "revision": "062070972fbad0f4fbb3505e6d013a39",
     "url": "/static/media/primeicons.06207097.woff"
   },
   {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/static/media/primeicons.acb19296.ttf"
+  },
+  {
     "revision": "1d854e3ab2c37173804a0d1d1af229ee",
     "url": "/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/static/media/primeicons.c08d00ca.eot"
   },
   {
     "revision": "8a9fa02cc30ea189e6fdbf02835ec87b",
@@ -64,7 +68,7 @@ self.__precacheManifest = [
     "url": "/static/media/Logo All Active IT met tekst licht geel.420e6395.png"
   },
   {
-    "revision": "d7ef0cca0229656c4d4c460aa409101f",
+    "revision": "c601d2e044135d67cdfc70c2168238f3",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77f157a41d7e496c74245d45655ccdc5.js"
+  "/precache-manifest.e832d3f8a54517be3f15ba8dc790bab2.js"
 );
 
 workbox.clientsClaim();
